0b8afa70398cacf74a6032284186a2c403baf90a
dvstefanov <d.stefanov088@gmail.com>
2024-03-09 17:15:33 +0200

    Add modular router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.set('views', 'src/views');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-	res.render('home/home');
-});
+app.use(routes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
