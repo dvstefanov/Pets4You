053102536fe4a9cde1ea2cf28dbb0ce3d4ecdf84
dvstefanov <d.stefanov088@gmail.com>
2024-03-09 19:27:34 +0200

    Render page title dynamically

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-	res.render('home/home');
+	res.render('home/home', { title: 'Home Page' });
 });
 
 router.get('/contacts', (req, res) => {
-    res.render('home/contacts');
+    res.render('home/contacts', { title: 'Contacts Page' });
 });
 
 module.exports = router;
